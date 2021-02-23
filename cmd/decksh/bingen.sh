2dbbf14565aabec78e4c5b6c04381fd1471bde6e
Anthony Starks <ajstarks@gmail.com>
2021-02-23 16:25:59 -0500

    add Apple Silicon binaries

---
+++ cmd/decksh/bingen.sh
@@ -0,0 +1,19 @@
+base="github.com/ajstarks/decksh/cmd"
+for b in decksh
+do
+	echo -n "$b - "
+	for o in "linux/amd64" "darwin/amd64" "darwin/arm64" "windows/amd64" "windows/386"
+	do
+		echo -n "$o "
+		goos=$(echo $o|cut -f1 -d /)
+		goarch=$(echo $o|cut -f2 -d /)
+		if test "$goos" = "windows"
+		then
+			exe="${goos}-${goarch}-${b}.exe"
+		else
+			exe="${goos}-${goarch}-${b}"
+		fi
+		GOOS=$goos GOARCH=$goarch go build -o $exe $base/$b
+	done
+	echo
+done
