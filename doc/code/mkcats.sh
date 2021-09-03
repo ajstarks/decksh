11033c9d5175ab3f51b81f737c5cd6a91ce02e5e
Anthony Starks <ajstarks@gmail.com>
2021-09-03 15:14:14 -0400

    doc: ref: use new color index script

---
+++ doc/code/mkcats.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./coloridx "Color Index"  colors  > color-index.dsh
+./coloridx "Neutrals"   neutrals  > neutrals.dsh
+./coloridx "Reds"          reds   > reds.dsh
+./coloridx "Greens"      greens   > greens.dsh
+./coloridx "Blues"        blues   > blues.dsh
+./coloridx "Violets"    violets   > violets.dsh
+./coloridx "Yellows"    yellows   > yellows.dsh
