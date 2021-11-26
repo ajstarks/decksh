becc9a7c67d2c27d11d5d20b463db6469fb6b941
Anthony Starks <ajstarks@gmail.com>
2021-11-26 14:44:18 -0500

    add CGO_ENABLED=0 for binaries

--- cmd/decksh/bingen.sh
+++ cmd/decksh/bingen.sh
@@ -13,7 +13,7 @@ do
 		else
 			exe="${goos}-${goarch}-${b}"
 		fi
-		GOOS=$goos GOARCH=$goarch go build -o binaries/$exe $base/$b
+		CGO_ENABLED=0 GOOS=$goos GOARCH=$goarch go build -o binaries/$exe $base/$b
 	done
 	echo
 done
