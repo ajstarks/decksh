f6a2ccd620fc4e6560aa873605eb8d3cdbfbdf5e
Anthony Starks <ajstarks@gmail.com>
2025-12-11 13:45:49 -0500

    re-org tests

--- roadtrip.d
+++
@@ -1,8 +0,0 @@
-geo:40.712778,-74.006111	New York
-geo:39.952778,-75.163611	Philadelphia
-geo:39.768611,-86.158056	Indianapolis
-geo:38.627222,-90.197778	St. Louis
-geo:35.468611,-97.521389	Oklahoma City
-geo:35.084444,-106.650278	Albuquerque
-geo:36.167222,-115.148611	Las Vegas
-geo:34.050000,-118.250000	Los Angeles
